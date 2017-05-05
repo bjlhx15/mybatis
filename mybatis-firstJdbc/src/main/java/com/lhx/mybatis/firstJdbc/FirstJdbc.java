f7e473addf73ba7212b95f70d6339bc9ac620de6
lihongxu <bjlhx15@163.com>
2017-05-05 22:23:27 +0800

    更新数据库密码

--- mybatis-firstJdbc/src/main/java/com/lhx/mybatis/firstJdbc/FirstJdbc.java
+++ mybatis-firstJdbc/src/main/java/com/lhx/mybatis/firstJdbc/FirstJdbc.java
@@ -23,7 +23,7 @@ public class FirstJdbc {
 
 			// 通过驱动管理类获取数据库链接
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis?characterEncoding=utf-8",
-					"root", "lhx963");
+					"root", "root");
 			// 定义sql语句 ?表示占位符
 			String sql = "select * from user where username = ?";
 			// 获取预处理statement
