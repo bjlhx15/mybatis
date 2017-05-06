d9d737b1e398cced604d08f989ef6ac19c2078a3
lihongxu <bjlhx15@163.com>
2017-05-06 22:20:33 +0800

    mybatis的基本使用

--- mybatis-firstJdbc/src/test/java/com/lhx/mybatis/po/UserTest.java
+++ mybatis-firstJdbc/src/test/java/com/lhx/mybatis/po/UserTest.java
@@ -14,84 +14,133 @@ import org.junit.Test;
 public class UserTest {
 
 	// 会话工厂
-		private SqlSessionFactory sqlSessionFactory;
+	private SqlSessionFactory sqlSessionFactory;
 
-		@Before
-		public void createSqlSessionFactory() throws IOException {
-			// 配置文件
-			String resource = "SqlMapConfig.xml";
-			InputStream inputStream = Resources.getResourceAsStream(resource);
-			// 使用SqlSessionFactoryBuilder从xml配置文件中创建SqlSessionFactory
-			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		}
+	@Before
+	public void createSqlSessionFactory() throws IOException {
+		// 配置文件
+		String resource = "SqlMapConfig.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		// 使用SqlSessionFactoryBuilder从xml配置文件中创建SqlSessionFactory
+		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
 
-		// 根据 id查询用户信息
-		@Test
-		public void testFindUserById() {
-			// 数据库会话实例
-			SqlSession sqlSession = null;
-			try {
-				// 创建数据库会话实例sqlSession
-				sqlSession = sqlSessionFactory.openSession();
-				// 查询单个记录，根据用户id查询用户信息
-				// 第一个参数mapping文件中的statement的id，等于namespace+。+statementId
-				// 第二个参数：指定映射文件中所匹配的parameterType累死那个的参数
-				// sqlSession.selectOne结果与映射所匹配的resultType类型的对象
-				User user = sqlSession.selectOne("test.findUserById", 1);
-				// 输出用户信息
-				System.out.println(user);
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				if (sqlSession != null) {
-					sqlSession.close();
-				}
+	// 根据 id查询用户信息
+	@Test
+	public void testFindUserById() {
+		// 数据库会话实例
+		SqlSession sqlSession = null;
+		try {
+			// 创建数据库会话实例sqlSession
+			sqlSession = sqlSessionFactory.openSession();
+			// 查询单个记录，根据用户id查询用户信息
+			// 第一个参数mapping文件中的statement的id，等于namespace+。+statementId
+			// 第二个参数：指定映射文件中所匹配的parameterType累死那个的参数
+			// sqlSession.selectOne结果与映射所匹配的resultType类型的对象
+			User user = sqlSession.selectOne("test.findUserById", 1);
+			// 输出用户信息
+			System.out.println(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (sqlSession != null) {
+				sqlSession.close();
 			}
 		}
+	}
 
-		// 根据用户名称模糊查询用户信息
-		@Test
-		public void testFindUserByUsername() {
-			// 数据库会话实例
-			SqlSession sqlSession = null;
-			try {
-				// 创建数据库会话实例sqlSession
-				sqlSession = sqlSessionFactory.openSession();
-				// 查询单个记录，根据用户id查询用户信息
-				List<User> list = sqlSession.selectList("test.findUserByName", "小明");
-				System.out.println(list.size());
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				if (sqlSession != null) {
-					sqlSession.close();
-				}
+	// 根据用户名称模糊查询用户信息
+	@Test
+	public void testFindUserByUsername() {
+		// 数据库会话实例
+		SqlSession sqlSession = null;
+		try {
+			// 创建数据库会话实例sqlSession
+			sqlSession = sqlSessionFactory.openSession();
+			// 查询单个记录，根据用户id查询用户信息
+			List<User> list = sqlSession.selectList("test.findUserByName", "小明");
+			System.out.println(list.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (sqlSession != null) {
+				sqlSession.close();
 			}
 		}
-		// 添加用户信息
-	    @Test
-	    public void testInsert() {
-	       // 数据库会话实例
-	       SqlSession sqlSession = null;
-	       try {
-	           // 创建数据库会话实例sqlSession
-	           sqlSession = sqlSessionFactory.openSession();
-	           // 添加用户信息
-	           User user = new User();
-	           user.setUsername("张小明");
-	           user.setAddress("河南郑州");
-	           user.setSex("1");
-	           //user.setPrice(1999.9f);
-	           sqlSession.insert("test.insertUser", user);
-	           //提交事务
-	           sqlSession.commit();
-	       } catch (Exception e) {
-	           e.printStackTrace();
-	       } finally {
-	           if (sqlSession != null) {
-	              sqlSession.close();
-	           }
-	       }
-	    }
+	}
 
+	// 添加用户信息
+	@Test
+	public void testInsert() {
+		// 数据库会话实例
+		SqlSession sqlSession = null;
+		try {
+			// 创建数据库会话实例sqlSession
+			sqlSession = sqlSessionFactory.openSession();
+			// 添加用户信息
+			User user = new User();
+			user.setUsername("张小明");
+			user.setAddress("河南郑州");
+			user.setSex("1");
+			// user.setPrice(1999.9f);
+			sqlSession.insert("test.insertUser", user);
+			// 提交事务
+			sqlSession.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (sqlSession != null) {
+				sqlSession.close();
+			}
+		}
+	}
+
+	// 根据id删除用户
+	@Test
+	public void testDelete() {
+		// 数据库会话实例
+		SqlSession sqlSession = null;
+		try {
+			// 创建数据库会话实例sqlSession
+			sqlSession = sqlSessionFactory.openSession();
+			// 删除用户
+			sqlSession.delete("test.deleteUserById", 18);
+			// 提交事务
+			sqlSession.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (sqlSession != null) {
+				sqlSession.close();
+			}
+		}
+	}
+
+	// 更新用户信息
+	@Test
+	public void testUpdate() {
+		// 数据库会话实例
+		SqlSession sqlSession = null;
+		try {
+			// 创建数据库会话实例sqlSession
+			sqlSession = sqlSessionFactory.openSession();
+			// 添加用户信息
+			User user = new User();
+			user.setId(16);
+			user.setUsername("张小明");
+			user.setAddress("河南郑州");
+			user.setSex("1");
+			//user.setPrice(1999.9f);
+			sqlSession.update("test.updateUser", user);
+			// 提交事务
+			sqlSession.commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (sqlSession != null) {
+				sqlSession.close();
+			}
+		}
+	}
 }
